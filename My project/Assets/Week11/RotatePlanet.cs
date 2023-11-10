7713624e4073765fca2b713fb4ec0a89b4bc7059
ybg1219 <12202057@inha.edu>
2023-11-10 16:06:52 +0900

    ar lecture1

---
+++ My project/Assets/Week11/RotatePlanet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+    public Transform tr;
+    public Transform target_tr;
+    public float rot_speed = 15.0f;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();   
+        // tr은 스크립트가 붙은 물체의 position이니까, 
+    }
+    void Update()
+    {
+        tr.RotateAround(target_tr.position, Vector3.up, Time.deltaTime * rot_speed);
+    }
+}
